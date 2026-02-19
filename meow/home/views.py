1c7cf9a129a1eeda333789e035a43e64cdad32bc
Shishir <bseeseer@gmail.com>
2026-02-19 13:27:21 +0545

    passing context

--- meow/home/views.py
+++ meow/home/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
+from django.shortcuts import render
 # Create your views here.
+def home(request):
+    peoples = [
+        {
+            'name': 'Shishir',
+            'age': 25,
+            'city': 'Kathmandu'
+        },
+
+
+        {
+            'name': 'Sita',
+            'age': 30,
+            'city': 'Lalitpur'
+        },
+
+
+        {
+            'name': 'Ram',
+            'age': 28,
+            'city': 'Bhaktapur'
+        }
+    ]
+    return render (request, 'home/index.html', context = {'peoples': peoples})
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def shishir(request):
+    peoples = [
+        {
+            'name': 'Shishir',
+            'age': 25,
+            'city': 'Kathmandu'
+        },
+
+
+        {
+            'name': 'Sita',
+            'age': 30,
+            'city': 'Lalitpur'
+        },
+
+
+        {
+            'name': 'Ram',
+            'age': 28,
+            'city': 'Bhaktapur'
+        }
+    ]
+    return render(request, 'home/index.html', )
+
+
+
