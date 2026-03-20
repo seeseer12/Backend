f03d6b569ba98a1425bb0ca5c6b9cfc139d4d8e1
Shishir <bseeseer@gmail.com>
2026-03-20 21:34:40 +0545

    added on march 20

--- meow/home/models.py
+++ meow/home/models.py
@@ -5,5 +5,7 @@ class student(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
     city=models.CharField(max_length=100,null=True,blank=True)
+
+    
     def __str__(self):
      return self.name
